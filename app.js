76776206ec10bf4dad626bd2850eefe315f2e4eb
yuanhao <yuanhao1@beiwaiguoji.com>
2020-04-16 12:32:55 +0800

    查看ip

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const upload = require('./router/upload');
 
 const app = new Koa();
 var router = new Router({
-    prefix: '/server'
+    prefix: '/server',
 });
 
 app.use(bodyParser());
@@ -29,15 +29,17 @@ app.use(async (ctx, next) => {
 // 具体参数我们在后面进行解释
 app.use(
     cors({
-        origin: function(ctx) {
+        origin: function (ctx) {
             // 服务器发送的请求中，请求头没有referer，通过ip来加入白名单
+            console.log(ctx.request.ip, '-ip---------------------');
             if (ctx.request.ip.includes('127.0.0.1')) {
                 ctx.header.referer = 'http://localhost:3001';
             }
             let whiteList = [
                 'http://localhost:3002',
                 'http://localhost:3001',
-                'http://192.168.1.6:3002'
+                'http://192.168.1.6:3002',
+                'http://yuanhao-web.cn',
             ];
             let url = ctx.header.referer.match(/(\w+):\/\/([^/:]+)(:\d*)?/)[0];
             if (whiteList.includes(url)) {
@@ -47,14 +49,14 @@ app.use(
         // exposeHeaders: ["WWW-Authenticate", "Server-Authorization"],
         maxAge: 5,
         credentials: true,
-        allowMethods: ['GET', 'POST', 'DELETE']
+        allowMethods: ['GET', 'POST', 'DELETE'],
         // allowHeaders: ["Content-Type", "Authorization", "Accept"]
     })
 );
 
 // 验证token
 app.use((ctx, next) => {
-    return next().catch(err => {
+    return next().catch((err) => {
         if (err.status === 401) {
             responseError(ctx, 401, '用户身份已过期，请重新登录！');
         } else {
@@ -65,10 +67,10 @@ app.use((ctx, next) => {
 
 app.use(
     koajwt({
-        secret: 'my_token'
+        secret: 'my_token',
     }).unless({
         // 白名单路径
-        path: [/\/user/, /\/blog\/client/, /\/upload/]
+        path: [/\/user/, /\/blog\/client/, /\/upload/],
     })
 );
 
