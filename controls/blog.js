0135edc4d87a0c38d453446d4ed8cb06f3d0ab20
yuanhaozzz <yuanhao_zzz@163.com>
2020-05-12 17:57:34 +0800

    提交

--- controls/blog.js
+++ controls/blog.js
@@ -1,4 +1,4 @@
-const { connectPool } = require('../sql/connect');
+const { connectPool } = require('../sql/connect')
 const {
     getArticleList,
     addArticle,
@@ -9,67 +9,69 @@ const {
     getClassifyArticleList,
     getAllArticleList,
     updateData
-} = require('../sql/sql');
+} = require('../sql/sql')
 const {
     responseSuccess,
     responseError,
     changeResponseFormat,
     changeReuestFormat
-} = require('../utils/common');
+} = require('../utils/common')
 
 /**
  * 获取热门文章列表
  */
 exports.getArticleList = async ctx => {
-    let getList = await connectPool(getArticleList(ctx.request.body));
-    let searchCount = await connectPool(searchArticleCount(ctx.request.body.type));
+    let getList = await connectPool(getArticleList(ctx.request.body))
+    let searchCount = await connectPool(
+        searchArticleCount(ctx.request.body.type)
+    )
     responseSuccess(ctx, {
         list: changeResponseFormat(getList),
         total: searchCount[0].total
-    });
-};
+    })
+}
 
 /**
  * 新增热门文章
  */
 exports.addArticle = async ctx => {
-    let options = ctx.request.body;
-    let getList = await connectPool(addArticle(changeReuestFormat(options)));
-    responseSuccess(ctx, {});
-};
+    let options = ctx.request.body
+    let getList = await connectPool(addArticle(changeReuestFormat(options)))
+    responseSuccess(ctx, {})
+}
 
 /**
  * 编辑热门文章
  */
 exports.editArticle = async ctx => {
-    let options = ctx.request.body;
-    let update = await connectPool(editArticle(changeReuestFormat(options)));
-    responseSuccess(ctx, {});
-};
+    let options = ctx.request.body
+    let update = await connectPool(editArticle(changeReuestFormat(options)))
+    responseSuccess(ctx, {})
+}
 
 /**
  * 获取热门文章详情
  *
  */
 exports.getArticleDetail = async ctx => {
-    let { articleId } = ctx.request.body;
-    let detail = await connectPool(getArticleDetail(articleId));
+    let { articleId } = ctx.request.body
+    let detail = await connectPool(getArticleDetail(articleId))
     if (detail.length > 0) {
-        responseSuccess(ctx, { detail: changeResponseFormat(detail[0]) });
+        responseSuccess(ctx, { detail: changeResponseFormat(detail[0]) })
     } else {
-        responseError(ctx, 0, '没有查询到文章');
+        responseError(ctx, 0, '没有查询到文章')
     }
-};
+}
 
 /**
  * 删除文章
  *
  */
 exports.deleteArticle = async ctx => {
-    let { articleId } = ctx.request.body;
-    let article = await connectPool(deleteArticle(articleId));
-    responseSuccess(ctx, {});
-};
+    let { articleId } = ctx.request.body
+    let article = await connectPool(deleteArticle(articleId))
+    responseSuccess(ctx, {})
+}
 
 // 客户端
 
@@ -77,29 +79,28 @@ exports.deleteArticle = async ctx => {
  * 首页文章列表
  */
 exports.getHomeList = async ctx => {
-    let common = { page: 1, pageSize: 8 };
-    let hotList = await connectPool(getClassifyArticleList(1));
-    let recommendList = await connectPool(getClassifyArticleList(2));
-    let list = await connectPool(getAllArticleList(common));
+    let {type} =  ctx.request.body
+    let common = { page: 1, pageSize: 8, type }
+    let hotList = await connectPool(getClassifyArticleList(1))
+    let list = await connectPool(getAllArticleList(common))
     responseSuccess(ctx, {
         hotList: changeResponseFormat(hotList),
-        recommendList: changeResponseFormat(recommendList),
         list: changeResponseFormat(list)
-    });
-};
+    })
+}
 
 /**
  * 首页文章列表
  */
 exports.getArticleDetail = async ctx => {
-    let { articleId } = ctx.request.body;
-    let detail = await connectPool(getArticleDetail(articleId));
+    let { articleId } = ctx.request.body
+    let detail = await connectPool(getArticleDetail(articleId))
     if (detail.length > 0) {
-        responseSuccess(ctx, { detail: changeResponseFormat(detail[0]) });
+        responseSuccess(ctx, { detail: changeResponseFormat(detail[0]) })
     } else {
-        responseError(ctx, 0, '没有查询到文章');
+        responseError(ctx, 0, '没有查询到文章')
     }
-};
+}
 
 /**
  *  更新数据库字段
@@ -107,7 +108,7 @@ exports.getArticleDetail = async ctx => {
 exports.updateData = async ctx => {
     let update = await connectPool(
         updateData(changeReuestFormat(ctx.request.body))
-    );
-    console.log(update, '==============');
-    responseSuccess(ctx, {});
-};
+    )
+    console.log(update, '==============')
+    responseSuccess(ctx, {})
+}
--- sql/sql.js
+++ sql/sql.js
@@ -2,24 +2,24 @@ let {
     getObjectKey,
     getObjectValue,
     updateTabledata
-} = require('../utils/common');
+} = require('../utils/common')
 
 /**
  * 查找是否注册
  * @parma {accountNumber} String 账号
  */
 exports.selectIsRegister = accountNumber => {
-    return `select * from user where account_number='${accountNumber}'`;
-};
+    return `select * from user where account_number='${accountNumber}'`
+}
 
 /**
  * 插入用户信息
  * @parma {options} Object 用户基本信息
  */
 exports.insertIntoUser = options => {
-    let { username, accountNumber, password, ip } = options;
-    return `insert into user value(null, '${username}', '${accountNumber}', '${password}', '${ip}', NOW(), NOW())`;
-};
+    let { username, accountNumber, password, ip } = options
+    return `insert into user value(null, '${username}', '${accountNumber}', '${password}', '${ip}', NOW(), NOW())`
+}
 
 /**
  * 验证用户账号密码
@@ -27,8 +27,8 @@ exports.insertIntoUser = options => {
  * @parma {ip} String 用户登录ip地址
  */
 exports.updateLoginInfo = (id, ip) => {
-    return `update user set ip='${ip}', login_time=NOW() where id=${id}`;
-};
+    return `update user set ip='${ip}', login_time=NOW() where id=${id}`
+}
 
 /**
  * 获取文章列表
@@ -36,11 +36,11 @@ exports.updateLoginInfo = (id, ip) => {
  */
 
 exports.getArticleList = options => {
-    let { type, page, pageSize } = options;
+    let { type, page, pageSize } = options
     return `select id, title, description, author, cover, image_url, page_views, release_time, type, online from article where type=${type}  order by release_time desc limit ${(page -
         1) *
-        pageSize}, ${pageSize}`;
-};
+        pageSize}, ${pageSize}`
+}
 
 /**
  * 新增文章
@@ -49,66 +49,68 @@ exports.getArticleList = options => {
 exports.addArticle = options => {
     return `insert into article (id, ${getObjectKey(
         options
-    )}) values(null,${getObjectValue(options)} )`;
-};
+    )}) values(null,${getObjectValue(options)} )`
+}
 
 /**
  * 获取文章详情
  * @param {Int} articleId   文章id
  */
 exports.getArticleDetail = articleId => {
-    return `select * from article where id=${articleId}`;
-};
+    return `select * from article where id=${articleId}`
+}
 
 /**
  * 编辑文章详情
  * @param {Object} options   修改字段
  */
 exports.editArticle = options => {
-    let { id } = options;
-    return `update article set ${updateTabledata(options)} where id=${id}`;
-};
+    let { id } = options
+    return `update article set ${updateTabledata(options)} where id=${id}`
+}
 
 /**
  * 删除文章
  * @param {Int} articleId   文章ID
  */
 exports.deleteArticle = articleId => {
-    return `delete from article where id=${articleId}`;
-};
+    return `delete from article where id=${articleId}`
+}
 
 /**
  * 查找文章数量
  * @param {Number} type 类型  1 热门文章 2 推荐文章
  */
 exports.searchArticleCount = type => {
-    return `select count(id) as total from article where type=${type}`;
-};
+    return `select count(id) as total from article where type=${type}`
+}
 
 /**
  * 获取分类文章列表
  * @param {Number} type   1 热门文章 2 推荐文章
  */
 exports.getClassifyArticleList = type => {
-    return `select id, title, description, author, cover, image_url, page_views, release_time, type, online from article where type=${type}  order by release_time desc limit 0, 8`;
-};
+    return `select id, title, description, author, cover, image_url, page_views, release_time, type, online from article where type=${type}  order by release_time desc limit 0, 8`
+}
 
 /**
  * 获取所有文章  时间降序
  * @param {Object} options 数据集合
  */
 exports.getAllArticleList = options => {
-    let { page, pageSize } = options;
-    return `select id, title, description, author, cover, image_url, page_views, release_time, type, online from article order by release_time desc limit ${(page -
+    let { page, pageSize, type } = options
+    // order by release_time desc
+    let string = ''
+    return `select id, title, description, author, cover, image_url, page_views, release_time, type, online from article ${string} limit ${(page -
         1) *
-        pageSize}, ${pageSize}`;
-};
+        pageSize}, ${pageSize}`
+}
 
 /**
  * 更新表字段
  * @param {Object} options 需要更新的字段
  */
 exports.updateData = options => {
-    let id = options.id;
-    return `update article set ${updateTabledata(options)} where id=${id}`;
-};
+    let id = options.id
+    return `update article set ${updateTabledata(options)} where id=${id}`
+}
