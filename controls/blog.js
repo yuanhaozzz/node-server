7cfaf32bb9d5326cde40f82b1cd1b187d7c02f9a
yuanhao <yuanhao1@beiwaiguoji.com>
2020-05-24 23:11:03 +0800

    提交

--- controls/blog.js
+++ controls/blog.js
@@ -1,4 +1,4 @@
-const { connectPool } = require('../sql/connect')
+const { connectPool } = require('../sql/connect');
 const {
     getArticleList,
     addArticle,
@@ -6,8 +6,8 @@ const {
     editArticle,
     deleteArticle,
     searchArticleCount,
-    getClassifyArticleList
-} = require('../sql/sql')
+    getClassifyArticleList,
+} = require('../sql/sql');
 const {
     getClientArticleList,
     updateData,
@@ -18,187 +18,213 @@ const {
     addLevel1Comment,
     addLevel2Comment,
     getCommentTwoLevelList,
-    updateLevel1Comment,
-    updateLevel2Comment
-} = require('../sql/clientSql')
+    getUpdateField,
+} = require('../sql/clientSql');
 const {
     responseSuccess,
     responseError,
     changeResponseFormat,
-    changeReuestFormat
-} = require('../utils/common')
+    changeReuestFormat,
+} = require('../utils/common');
 
 /**
  * 获取热门文章列表
  */
-exports.getArticleList = async ctx => {
-    let getList = await connectPool(getArticleList(ctx.request.body))
+exports.getArticleList = async (ctx) => {
+    let getList = await connectPool(getArticleList(ctx.request.body));
     let searchCount = await connectPool(
         searchArticleCount(ctx.request.body.type)
-    )
+    );
     responseSuccess(ctx, {
         list: changeResponseFormat(getList),
-        total: searchCount[0].total
-    })
-}
+        total: searchCount[0].total,
+    });
+};
 
 /**
  * 新增热门文章
  */
-exports.addArticle = async ctx => {
-    let options = ctx.request.body
-    let getList = await connectPool(addArticle(changeReuestFormat(options)))
-    responseSuccess(ctx, {})
-}
+exports.addArticle = async (ctx) => {
+    let options = ctx.request.body;
+    let getList = await connectPool(addArticle(changeReuestFormat(options)));
+    responseSuccess(ctx, {});
+};
 
 /**
  * 编辑热门文章
  */
-exports.editArticle = async ctx => {
-    let options = ctx.request.body
-    let update = await connectPool(editArticle(changeReuestFormat(options)))
-    responseSuccess(ctx, {})
-}
+exports.editArticle = async (ctx) => {
+    let options = ctx.request.body;
+    let update = await connectPool(editArticle(changeReuestFormat(options)));
+    responseSuccess(ctx, {});
+};
 
 /**
  * 获取热门文章详情
  *
  */
-exports.getArticleDetail = async ctx => {
-    let { articleId } = ctx.request.body
-    let detail = await connectPool(getArticleDetail(articleId))
+exports.getArticleDetail = async (ctx) => {
+    let { articleId } = ctx.request.body;
+    let detail = await connectPool(getArticleDetail(articleId));
     if (detail.length > 0) {
-        responseSuccess(ctx, { detail: changeResponseFormat(detail[0]) })
+        responseSuccess(ctx, { detail: changeResponseFormat(detail[0]) });
     } else {
-        responseError(ctx, 0, '没有查询到文章')
+        responseError(ctx, 0, '没有查询到文章');
     }
-}
+};
 
 /**
  * 删除文章
  *
  */
-exports.deleteArticle = async ctx => {
-    let { articleId } = ctx.request.body
-    let article = await connectPool(deleteArticle(articleId))
-    responseSuccess(ctx, {})
-}
+exports.deleteArticle = async (ctx) => {
+    let { articleId } = ctx.request.body;
+    let article = await connectPool(deleteArticle(articleId));
+    responseSuccess(ctx, {});
+};
 
 // 客户端
 //--------------------------------------------------------------------
 /**
  * 首页文章列表
  */
-exports.getHomeList = async ctx => {
-    let params = Object.assign({ page: 1, pageSize: 8 }, ctx.request.body)
-    let hotList = await connectPool(getClassifyArticleList(1))
-    let list = await connectPool(getClientArticleList(params))
+exports.getHomeList = async (ctx) => {
+    let params = Object.assign({ page: 1, pageSize: 8 }, ctx.request.body);
+    let hotList = await connectPool(getClassifyArticleList(1));
+    let list = await connectPool(getClientArticleList(params));
     responseSuccess(ctx, {
         hotList: changeResponseFormat(hotList),
-        list: changeResponseFormat(list)
-    })
-}
+        list: changeResponseFormat(list),
+    });
+};
 
 /**
  * 文章详情
  */
-exports.getArticleDetail = async ctx => {
-    let { articleId } = ctx.request.body
-    let detail = await connectPool(getArticleDetail(articleId))
-    let commentList = await connectPool(getCommentList(articleId))
-    for(var i = 0; i < commentList.length; i++) {
-        let item = commentList[i]
-        item.subComment = changeResponseFormat(await connectPool(getCommentTwoLevelList(item.id)))
+exports.getArticleDetail = async (ctx) => {
+    let { articleId } = ctx.request.body;
+    let detail = await connectPool(getArticleDetail(articleId));
+    let commentList = await connectPool(getCommentList(articleId));
+    for (var i = 0; i < commentList.length; i++) {
+        let item = commentList[i];
+        item.subComment = changeResponseFormat(
+            await connectPool(getCommentTwoLevelList(item.id))
+        );
     }
     if (detail.length > 0) {
-        responseSuccess(ctx, { detail: changeResponseFormat(detail[0]), commentList: changeResponseFormat(commentList) })
+        responseSuccess(ctx, {
+            detail: changeResponseFormat(detail[0]),
+            commentList: changeResponseFormat(commentList),
+        });
     } else {
-        responseError(ctx, 0, '没有查询到文章')
+        responseError(ctx, 0, '没有查询到文章');
     }
-}
+};
 
 /**
  *  更新数据库字段
  */
-exports.updateData = async ctx => {
+exports.updateData = async (ctx) => {
     let update = await connectPool(
         updateData(changeReuestFormat(ctx.request.body))
-    )
-    responseSuccess(ctx, {})
-}
+    );
+    responseSuccess(ctx, {});
+};
 
 /**
  *  统计
  */
-exports.getStatisticst = async ctx => {
-    let statisticst = await connectPool(getStatisticst())
-    await connectPool(updateStatisticst({views: ++statisticst[0].views}))
-    statisticst = await connectPool(getStatisticst())
+exports.getStatisticst = async (ctx) => {
+    let statisticst = await connectPool(getStatisticst());
+    await connectPool(updateStatisticst({ views: ++statisticst[0].views }));
+    statisticst = await connectPool(getStatisticst());
     // 文章数量
-    let count = await connectPool(getArticleCount())
-    statisticst[0].article = count[0].count
+    let count = await connectPool(getArticleCount());
+    statisticst[0].article = count[0].count;
     responseSuccess(ctx, {
-        statisticst: statisticst[0]
-    })
-}
+        statisticst: statisticst[0],
+    });
+};
 
 /**
  *  更新统计
  */
-exports.updateStatisticst = async ctx => {
-    let statisticst = await connectPool(getStatisticst())
-    let params = ctx.request.body
+exports.updateStatisticst = async (ctx) => {
+    let statisticst = await connectPool(getStatisticst());
+    let params = ctx.request.body;
     if (params.favorite) {
-        params.favorite = ++statisticst[0].favorite
+        params.favorite = ++statisticst[0].favorite;
     }
-    await connectPool(updateStatisticst(params))
-    responseSuccess(ctx, {})
-}
-
+    await connectPool(updateStatisticst(params));
+    responseSuccess(ctx, {});
+};
 
 /**
  * 获取评论列表
  */
-exports.getCommentList = async ctx => {
-    
-    let list = await connectPool(getCommentList(ctx.body.articleId))
-    responseSuccess(ctx, {list})
-}
+exports.getCommentList = async (ctx) => {
+    let list = await connectPool(getCommentList(ctx.body.articleId));
+    responseSuccess(ctx, { list });
+};
 
 /**
  * 添加评论
  */
-exports.addComment = async ctx => {
-   
-    let params = Object.assign({}, ctx.request.body)
-    let comment = {id:  ctx.request.body.articleId,comment: ctx.request.body.comment}
+exports.addComment = async (ctx) => {
+    let params = Object.assign({}, ctx.request.body);
+    let comment = {
+        id: ctx.request.body.articleId,
+        comment: ctx.request.body.comment,
+    };
 
-    delete params.comment
-    await connectPool(updateData(changeReuestFormat(comment)))
+    delete params.comment;
 
-     // type 1 一级评论  2 二级评论
-     let {type} = params
-     if (type === 1) {
-        delete params.type
-        await connectPool(addLevel1Comment(changeReuestFormat(params)))
-     } else {
-        delete params.type
-        await connectPool(addLevel2Comment(changeReuestFormat(params)))
-     }
-    responseSuccess(ctx, {})
-}
+    // type 1 一级评论  2 二级评论
+    let { type } = params;
+    if (type === 1) {
+        // 更新文章列表评论数量
+        await connectPool(updateData('article', changeReuestFormat(comment)));
+
+        delete params.type;
+        await connectPool(addLevel1Comment(changeReuestFormat(params)));
+    } else {
+        delete params.type;
+        await connectPool(addLevel2Comment(changeReuestFormat(params)));
+    }
+    responseSuccess(ctx, {});
+};
 
 /**
  * 更新评论字段
  */
-exports.updateComment = async ctx => {
-    delete params.comment
-    let params = ctx.request.body
-    if (params.type === 1) {
-        await connectPool(updateLevel1Comment(changeReuestFormat(params)))
+exports.updateComment = async (ctx) => {
+    let params = ctx.request.body;
+    console.log(params, '-------------');
+    let comment = await connectPool(
+        getUpdateField(
+            params.type === 1 ? 'level_one_comment' : 'level_two_comment',
+            changeReuestFormat(params)
+        )
+    );
+    console.log(comment, '===================');
+    if (params.favorite) {
+        comment[0].favorite += 1;
     } else {
-        await connectPool(updateLevel2Comment(changeReuestFormat(params)))
+        comment[0].favorite -= 1;
+        if (comment[0].favorite <= 0) {
+            omment[0].favorite = 0;
+        }
     }
- 
-    responseSuccess(ctx, {})
-}
+
+    if (params.type === 1) {
+        await connectPool(
+            updateData('level_one_comment', changeReuestFormat(comment[0]))
+        );
+    } else {
+        await connectPool(
+            updateData('level_two_comment', changeReuestFormat(comment[0]))
+        );
+    }
+
+    responseSuccess(ctx, {});
+};
--- sql/clientSql.js
+++ sql/clientSql.js
@@ -1,39 +1,39 @@
 let {
     updateTabledata,
     getObjectKey,
-    getObjectValue
-} = require('../utils/common')
+    getObjectValue,
+} = require('../utils/common');
 /**
  * 获取文章
  * @param {Object} options 数据集合
  */
-exports.getClientArticleList = options => {
-    let {page, pageSize, type} = options
+exports.getClientArticleList = (options) => {
+    let { page, pageSize, type } = options;
     // order by release_time desc
     // 查询字段
-    let queryDbField = ' id, title, description, author, cover, image_url, page_views, release_time, type, online, likes, comment from article '
+    let queryDbField =
+        ' id, title, description, author, cover, image_url, page_views, release_time, type, online, likes, comment from article ';
     // 查询条数
-    let pagination = `${(page - 1) * pageSize}, ${pageSize}`
-    switch(type) {
+    let pagination = `${(page - 1) * pageSize}, ${pageSize}`;
+    switch (type) {
         case 0:
             // 所有
-            return getAllArticleList(queryDbField, pagination)
+            return getAllArticleList(queryDbField, pagination);
         case 1:
             // 最新
-            return getNewestArticleList(queryDbField, pagination)
+            return getNewestArticleList(queryDbField, pagination);
         case 2:
             // 最热
-            return getHotArticleList(queryDbField, pagination)
+            return getHotArticleList(queryDbField, pagination);
         case 3:
         case 4:
             // 分类  源码 面试题
-            return getCategoryArticleList(queryDbField, pagination, options)
+            return getCategoryArticleList(queryDbField, pagination, options);
         case 5:
             // 关键字
-            return getKeyWordArticleList(queryDbField, pagination, options)
+            return getKeyWordArticleList(queryDbField, pagination, options);
     }
-   
-}
+};
 
 /**
  * 获取所有文章
@@ -41,8 +41,8 @@ exports.getClientArticleList = options => {
  * @param {String} page  分页
  */
 getAllArticleList = (field, page) => {
-    return `select ${field} limit ${page}`
-}
+    return `select ${field} limit ${page}`;
+};
 
 /**
  * 获取最新文章
@@ -50,8 +50,8 @@ getAllArticleList = (field, page) => {
  * @param {String} page  分页
  */
 getNewestArticleList = (field, page) => {
-    return `select ${field} order by release_time desc limit ${page}` 
-}
+    return `select ${field} order by release_time desc limit ${page}`;
+};
 
 /**
  * 获取最热文章
@@ -59,8 +59,8 @@ getNewestArticleList = (field, page) => {
  * @param {String} page  分页
  */
 getHotArticleList = (field, page) => {
-    return `select ${field} order by page_views desc limit ${page}` 
-}
+    return `select ${field} order by page_views desc limit ${page}`;
+};
 
 /**
  * 获取分类文章   源码  面试题
@@ -69,9 +69,9 @@ getHotArticleList = (field, page) => {
  * @param {Object} options  请求参数  type 3 源码 4 面试题
  */
 getCategoryArticleList = (field, page, options) => {
-    let {type} = options
-    return `select ${field} where type=${type} order by page_views desc limit ${page}` 
-}
+    let { type } = options;
+    return `select ${field} where type=${type} order by page_views desc limit ${page}`;
+};
 
 /**
  * 搜索关键词
@@ -80,78 +80,86 @@ getCategoryArticleList = (field, page, options) => {
  * @param {Object} options  keyword 关键词
  */
 getKeyWordArticleList = (field, page, options) => {
-    let {keyword} = options
-    console.log(keyword)
-    return `select * from article where title like '%${keyword}%'` 
-}
-
+    let { keyword } = options;
+    console.log(keyword);
+    return `select * from article where title like '%${keyword}%'`;
+};
 
 /**
  * 更新表字段
- * @param {Object} options 需要更新的字段
+ * @param {String} table        表名
+ * @param {Object} options      需要更新的字段
  */
-exports.updateData = options => {
-    let id = options.id
-    return `update article set ${updateTabledata(options)} where id=${id}`
-}
+exports.updateData = (table = 'article', options) => {
+    console.log(options);
+    let id = options.id;
+    return `update ${table} set ${updateTabledata(options)} where id=${id}`;
+};
 
 /**
  * 获取首页信息
  */
 exports.getStatisticst = () => {
-    return `select * from statistics`
-}
+    return `select * from statistics`;
+};
 
 /**
  * 更新首页信息
  */
-exports.updateStatisticst = options => {
-    return `update statistics set ${updateTabledata(options)}`
-}
+exports.updateStatisticst = (options) => {
+    return `update statistics set ${updateTabledata(options)}`;
+};
 
 /**
  * 获取文章数量
  */
 exports.getArticleCount = () => {
-    return `select count(id) as count from article`
-}
+    return `select count(id) as count from article`;
+};
 
 /**
  * 获取评论列表
  */
-exports.getCommentList = articleId => {
-    return `select * from level_one_comment where article_id=${articleId} order by create_time desc`
-}
+exports.getCommentList = (articleId) => {
+    return `select * from level_one_comment where article_id=${articleId} order by create_time desc`;
+};
 
 /**
  * 获取二级评论列表
  */
-exports.getCommentTwoLevelList = parentId => {
-    return `select * from level_two_comment where parent_id=${parentId} order by create_time desc`
-}
+exports.getCommentTwoLevelList = (parentId) => {
+    return `select * from level_two_comment where parent_id=${parentId} order by create_time desc`;
+};
 
 /**
  * 添加一级评论
  * @param {Object} params 插入字段
  */
-exports.addLevel1Comment = params => {
-    return `insert into  level_one_comment (id, ${getObjectKey(params)}) values(null, ${getObjectValue(params)})`
-}
+exports.addLevel1Comment = (params) => {
+    console.log(params, '====');
+    return `insert into  level_one_comment (id, ${getObjectKey(
+        params
+    )}) values(null, ${getObjectValue(params)})`;
+};
 
 /**
  * 添加二级评论
  * @param {Object} params 插入字段
  */
-exports.addLevel2Comment = params => {
-    return `insert into  level_two_comment (id, ${getObjectKey(params)}) values(null, ${getObjectValue(params)})`
-}
-
+exports.addLevel2Comment = (params) => {
+    console.log(params, '-------------');
+    return `insert into  level_two_comment (id, ${getObjectKey(
+        params
+    )}) values(null, ${getObjectValue(params)})`;
+};
 
 /**
- * 更新一级评论
- * @param {Object} params 插入字段
+ * 获取更新字段
+ * @param {String} table    表
+ * @param {Object} options  参数选项
  */
-exports.updateLevel1Comment = params => {
-    return `update level_one_comment set ${updateTabledata(params)}`
-}
-
+exports.getUpdateField = (table, options) => {
+    return `select ${getObjectKey(options)} from ${table} where id=${
+        options.id
+    }`;
+};
