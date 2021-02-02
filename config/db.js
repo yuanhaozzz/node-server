2e5da0ec9a3bd5bc5f51bf60e9a9f1d9cd2ae464
yuanhaozzz <yuanhao_zzz@163.com>
2021-02-02 15:56:06 +0800

    修改密码

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ module.exports = {
     host: '132.232.1.48',
     port: 3306,
     user: 'root',
-    password: '_Yh3926909',
+    password: 'Yh@123456',
     database: 'server',
     timezone: '08:00'
 };
